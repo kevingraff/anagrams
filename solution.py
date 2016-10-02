8a251235c3473a8f0b0b03035d74a997ac5445cb
Kevin Graff <kgraff@gmail.com>
2016-10-02 17:57:30 -0400

    Added exception handling for file input Added function description string Simplified output formatting Changed file read block to use with open code

--- solution.py
+++ solution.py
@@ -5,35 +5,48 @@
 # 	Spaces are ignored
 #	Apostrophes are ignored
 #	All letters are treated as lowercase
+#	Dictionary is in the format of one word per line
 
 import sys, re
 from collections import defaultdict
-
-# if no arg provided display help and exit
-
-# read dict line by line
-	# >3 letters tolower then sort the letters
-	# add word to dictionary key = sorted letters
-
-# read dictionary keys
-	# if number of values >= keylength
-	# print the values
 	
-def cleanWord(word):
-	w = re.sub('\W', '', word.lower(), count=0) # Remove all non-alphanumeric characters from word and make it lower case
+def make_key(word):
+	'''Remove all non-alphanumeric characters from word and make it
+	lower case then sort the letters.
+	
+	Keyword arguments:
+	word -- string to operate on
+	
+	Returns: cleaned string with letters sorted
+	'''
+	w = re.sub('[\W\n]', '', word.lower(), count=0)
 	w = ''.join(sorted(w))
 	return w
-	
-dictionary = open(sys.argv[1]).read().splitlines()
 
-keyedList = defaultdict(list) # Initialize the sorted-letter key:value list
+# Quit if no dictionary is given
+if len(sys.argv) <= 1:
+	print("\tsolution.py: Please provide a dictionary as an argument")
+	sys.exit()
 
-for word in dictionary:
-	keyWord = cleanWord(word.strip())
-	
-	if len(keyWord) >= 4:
-		keyedList[keyWord].append(word)
+# Initialize the sorted-letter key:value list
+keyed_list = defaultdict(list)
 
-for key, val in keyedList.items():
+try:
+	with open(sys.argv[1]) as dictionary:
+		# Iterate through the dictionary file
+		for word in dictionary:
+			# Remove newlines and turn word into a hash key
+			key_word = make_key(word.rstrip())
+			# Ignore words less than 4 characters after removing apostrophes
+			if len(key_word) >= 4:
+				# Append word to the hash
+				keyed_list[key_word].append(word.rstrip())
+except IOError:
+	print("\tsolution.py: Error, invalid file")
+
+# Iterate through the hash pulling each key and value
+for key, val in keyed_list.items():
+	# For key length n, ignore any entries with fewer than n anagrams
 	if len(val) >= len(key):
-		print(', '.join(val))		
+		# Format the output
+		print(", ".join(val))
